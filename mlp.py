69f9d151987f1e9d945a17a42621a6ae8b261c56
Lucas Lyon de Azevedo <lucas.lyon@dca.ufrn.br>
2018-05-25 16:36:47 -0300

    atualizado

--- mlp.py
+++ mlp.py
@@ -42,8 +42,8 @@ Passo_aprendizado = 0.5
 #Entradas = []
 #Saida_esperada = []
 #Teste =[]
-Entradas = [0,0.5]
-Saida_esperada = [0,1,0]
+Entradas = np.array([[-1],[0],[0.6]])
+Saida_esperada = np.array([[0],[1],[0]]) 
 
 
 class Rede_neural(object):
@@ -56,17 +56,17 @@ class Rede_neural(object):
 		#Pesos inicializados aleatoriamente
 		self.W1 = np.random.randn(self.TamEntrada,self.TamOculta) #Camada de entrada
 		self.W2 = np.random.randn(self.TamOculta,self.TamSaida) #Camada oculta
-		print(self.W1[0])
-		print(self.W1[1])
-		print("___________________________")
-		print(self.W2[0])
-		print(self.W2[1])
-		print(self.W2[2])
-	#Passo para frente
+		#Passo para frente
 	def Passo_Frente(self,X):
-		self.z = np.dot(X,self.W1)
+		self.z = X.T*self.W1
+	#	print("z")
+	#	print(self.z)
 		self.z2 = self.func_at(self.z)
+	#	print("z2")
+	#	print(self.z2)
 		self.z3 = np.dot(self.z2,self.W2)
+	#	print("z3")
+	#	print(self.z3)
 		saida = self.func_at(self.z3)
 		return saida
 
@@ -79,19 +79,39 @@ class Rede_neural(object):
 
 	def Passo_tras(self,X,Y,saida):
 		#Camada de saida
-		self.saida_erro = Y - saida
+		#print(Y)
+		#print(saida)
+		self.saida_erro = Y.T - saida
 		self.gradiente = self.saida_erro*self.func_at_derivada(saida)
 
+		self.W2 += Passo_aprendizado*(self.z2.dot(self.gradiente))
 		#Camada oculta
-		self.z2_erro = self.gradiente.dot(self.W2.T)
-		self.gradiente_oculto =  self.z2_erro * self.func_at_derivada(self.z2)
 		
-		self.W1 += Passo_aprendizado*(X.T.dot(self.gradiente_oculto))
-		self.W2 += Passo_aprendizado*(self.z2.T.dot(self.gradiente))
+		self.z2_erro = self.gradiente.T.dot(self.W2)
+		self.gradiente_oculto =  self.z2_erro * self.func_at_derivada(self.z2)
+		self.W1 += Passo_aprendizado*(self.gradiente_oculto.dot(X.T))
+		
+
+		'''	
+		print("X normal")
+		print(X)
+		print("X transposta")
+		print(X.transpose())
+		print("gradiente_oculto normal")
+		print(self.gradiente_oculto)
+		print("gradiente_oculto transposta")
+		print(self.gradiente_oculto.transpose())
+		'''	
+		
 	
+		
+		print("aqui")
+		print(self.gradiente)
+		print("separaS")
+		print(self.z2)
 	def treinamento(self,X,Y):
 		saida = self.Passo_Frente(X)
-		self.Passo_tras(X,Y,saida)
+		self.Passo_tras(X.T,Y,saida.T)
 
 if __name__ == '__main__':
 	
@@ -101,13 +121,19 @@ if __name__ == '__main__':
 
 	X = array(Entradas)
 	Y = array(Saida_esperada)
-	X = X/np.amax(X, axis=0)
-	Y = Y/np.amax(Y, axis=0)
+	#X = X/np.amax(X, axis=0)
+	#Y = Y/np.amax(Y, axis=0)
 
 	Rede_TOP = Rede_neural()
 	erro = []
-	for i in range(100000):
+	#print(Y)
+	Y= Y.transpose()
+	#print(X)
+	for i in range(1):
+
 		erro.append(np.sum((np.mean(np.square(Y-Rede_TOP.Passo_Frente(X))))))
+		print(Y)
+		print(X)
 		Rede_TOP.treinamento(X,Y)
 
 	#plt.plot(erro)
